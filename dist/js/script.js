4a27bb41cd4e644257363c643c6a3e205ac079e0
TsybankovaM <maruska-@inbox.ru>
2022-07-31 20:10:14 +0300

    added 768px adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,3 +5,21 @@ const slider = document.querySelector('.form__slider');
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+  menuItem = document.querySelectorAll('.menu__item'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu_active');
+      })
+  })
+})
